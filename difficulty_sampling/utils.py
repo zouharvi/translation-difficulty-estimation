8d6b55d6d765e26e936b96ebc9e66536fd5e8f8d
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-01-31 17:16:00 +0100

    expose one more data loader method

--- difficulty_sampling/utils.py
+++ difficulty_sampling/utils.py
@@ -5,3 +5,4 @@ We piggy-back on top of subset2evaluate for now. This might change later.
 import subset2evaluate.utils
 
 load_data_wmt_all = subset2evaluate.utils.load_data_wmt_all
+load_data_wmt = subset2evaluate.utils.load_data_wmt
