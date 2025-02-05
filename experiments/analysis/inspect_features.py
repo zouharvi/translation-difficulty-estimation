878b944491783b4b5fb8c7b9d8ec1db5add2ce13
sted19 <perrella@diag.uinroma1.it>
2025-02-05 14:52:25 +0100

    implement Sentinel_SRC feature and color systems in the analysis plots

--- experiments/analysis/inspect_features.py
+++ experiments/analysis/inspect_features.py
@@ -7,6 +7,7 @@ import scipy.stats
 import subset2evaluate.select_subset
 
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from tqdm import tqdm
 
 import numpy as np
@@ -123,7 +124,13 @@ class Features:
         4) Plot the feature using the plot_feature method
     """
 
-    available_features = ["src_length", "src_NE_count", "cometsrc_avg", "cometsrc_diversity"]
+    available_features = [
+        "src_length",
+        "src_NE_count",
+        "cometsrc_avg",
+        "cometsrc_diversity",
+        "sentinel_src",
+    ]
 
     def __init__(self, data: Data):
         self.data = data
@@ -160,7 +167,9 @@ class Features:
                     sample["scores"][system][feature] = counts[idx]["count"]
 
         elif feature == "cometsrc_avg":
-            data_new = subset2evaluate.select_subset.basic(method="cometsrc_avg", data=self.data.data)
+            data_new = subset2evaluate.select_subset.basic(
+                method="cometsrc_avg", data=self.data.data
+            )
             data_new = {
                 sample["i"]: sample["subset2evaluate_utility"] for sample in data_new
             }
@@ -169,16 +178,62 @@ class Features:
                     sample["scores"][system]["cometsrc_avg"] = data_new[sample["i"]]
 
         elif feature == "cometsrc_diversity":
-            data_new = subset2evaluate.select_subset.basic(method="cometsrc_diversity", data=self.data.data)
+            data_new = subset2evaluate.select_subset.basic(
+                method="cometsrc_diversity", data=self.data.data
+            )
             data_new = {
                 sample["i"]: sample["subset2evaluate_utility"] for sample in data_new
             }
             for sample in self.data.data:
                 for system in sample["scores"]:
-                    sample["scores"][system]["cometsrc_diversity"] = data_new[sample["i"]]
+                    sample["scores"][system]["cometsrc_diversity"] = data_new[
+                        sample["i"]
+                    ]
 
+        elif feature == "sentinel_src":
+            # to use, you need to install the sentinel_metric package following the readme of
+            # https://github.com/SapienzaNLP/guardians-mt-eval
+            from sentinel_metric import download_model, load_from_checkpoint
 
-    def plot_feature(self, feature: str):
+            model_path = download_model("sapienzanlp/sentinel-src-mqm")
+            model = load_from_checkpoint(model_path)
+
+            sources = [{"src": sample["src"]} for sample in self.data.data]
+            scores = model.predict(sources, batch_size=32, gpus=1).scores
+
+            assert len(scores) == len(self.data.data)
+
+            for idx, sample in enumerate(self.data.data):
+                for system in sample["scores"]:
+                    sample["scores"][system]["sentinel_src"] = scores[idx]
+
+    def filter_outliers(self, x, y):
+        """
+        Filter outliers (in terms of the feature x) from the data
+
+        Args:
+            x (List): Feature values
+            y (List): Human scores
+
+        Returns:
+            List: Filtered feature values
+            List: Filtered human scores
+        """
+
+        x = np.array(x)
+        y = np.array(y)
+
+        # Filter outliers
+        z = np.abs(scipy.stats.zscore(x))
+        x = x[z < 3]
+        y = y[z < 3]
+        sys_names = [sys_names[idx] for idx in range(len(z)) if z[idx] < 3]
+
+        assert len(x) == len(y) == len(sys_names)
+
+        return x, y, sys_names
+
+    def plot_feature(self, feature: str, filter_feature_outliers=False):
         """
         Plot a feature against the human scores
 
@@ -194,28 +249,49 @@ class Features:
         if not datadir.exists():
             datadir.mkdir(parents=True)
 
-        x = [
-            sample["scores"][system][feature]
-            for sample in data
-            for system in sample["scores"]
-        ]
-        y = [
-            sample["scores"][system]["human"]
-            for sample in data
-            for system in sample["scores"]
-        ]
+        # Collect x, y, and the system labels in parallel
+        x = []
+        y = []
+        sys_names = []
+        for sample in data:
+            for system in sample["scores"]:
+                x.append(sample["scores"][system][feature])
+                y.append(sample["scores"][system]["human"])
+                sys_names.append(system)
+
+        if filter_feature_outliers:
+            x, y, sys_names = self.filter_outliers(x, y, sys_names)
 
         fig, ax = plt.subplots(figsize=(6, 4), dpi=72)
 
+        # Color each system with a different color
+        unique_sys_names = sorted(set(sys_names))
+        system_to_id = {sys: idx for idx, sys in enumerate(unique_sys_names)}
+        cmap = plt.get_cmap("tab20")
+        point_colors = [cmap(system_to_id[sys_name]) for sys_name in sys_names]
+
         scatter = ax.scatter(
-            x, y, alpha=0.6,
+            x,
+            y,
+            c=point_colors,
+            alpha=0.9,
             edgecolors="w",
             s=10,
             linewidth=0,
             rasterized=True,
         )
 
-        ax.grid(True, linestyle="--", alpha=0.6)
+        # scatter an empty set of points to automatically create handles for the legend
+        for sys_name, color_id in system_to_id.items():
+            ax.scatter([], [], color=point_colors[color_id], label=sys_name)
+
+        ax.legend(
+            loc="best",
+            fontsize="x-small",
+            markerscale=0.5,
+            labelspacing=0.2,
+            borderpad=0.3,
+        )
 
         slope, intercept, r_value, p_value, std_err = linregress(x, y)
         line = slope * np.array(x) + intercept
@@ -224,12 +300,13 @@ class Features:
 
         corr_pearson = scipy.stats.pearsonr(x, y)[0]
         corr_spearman = scipy.stats.spearmanr(x, y)[0]
+
         plt.text(
             0.95,
             0.05,
-            f"Pearson: {corr_pearson:.1%}\nSpearman: {corr_spearman:.1%}".replace("%", r"\%"),
+            f"Pearson: {round(corr_pearson,2):.2f}\nSpearman: {round(corr_spearman, 2):.2f}",
             transform=ax.transAxes,
-            fontsize=12,
+            fontsize=10,
             verticalalignment="bottom",
             horizontalalignment="right",
             bbox=dict(facecolor="white", alpha=0.8),
@@ -237,7 +314,7 @@ class Features:
 
         ax.set_xlabel(feature, fontsize=16)
         ax.set_ylabel(f"{protocol} score", fontsize=16)
-
+        ax.grid(True, linestyle="--", alpha=0.6)
         fig.tight_layout(pad=0)
 
         plt_name = f"{protocol}.png"
@@ -282,6 +359,12 @@ if __name__ == "__main__":
         help="Feature to be computed",
     )
 
+    argparser.add_argument(
+        "--filter-feature-outliers",
+        action="store_true",
+        help="Filter outliers in the feature values",
+    )
+
     args = argparser.parse_args()
 
     data = Data.load(
@@ -293,4 +376,4 @@ if __name__ == "__main__":
 
     features = Features(data)
     features.measure_feature(args.feature)
-    features.plot_feature(args.feature)
+    features.plot_feature(args.feature, args.filter_feature_outliers)
