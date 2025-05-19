c601aa78d4831e205eb68a3064fa70188cfb291f
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-05-19 17:23:42 +0200

    compute diversity on wmt25

---
+++ experiments/03-post_effect_wmt25.py
@@ -0,0 +1,242 @@
+# %%
+
+import json
+import sentence_transformers
+import numpy as np
+import tqdm
+import difficulty_sampling.data
+import subsampling.misc
+import subsampling.syntactic_complexity
+import subsampling.average_word_frequency
+import matplotlib.pyplot as plt
+import difficulty_sampling.utils
+import scipy.stats
+import subsampling.sentinel
+from pathlib import Path
+
+with open("../data/wmt25.jsonl", "r") as f:
+    data_raw = [
+        {
+            "src": json.loads(x),
+            "scores": {"mock": {}},
+        }
+        for x in f.readlines()
+    ]
+
+data_all = difficulty_sampling.data.Data(
+    lp2src_data_list={"en": data_raw},
+    lps=["en"],
+    dataset_name="wmt25",
+    protocol=None,
+    domains="news",
+)
+
+
+import language_tool_python
+
+grammarcheck = language_tool_python.LanguageTool("en-US")
+src2error = list(
+    {
+        line["src"]
+        for data_name, data in data_all.lp2src_data_list.items()
+        for line in data
+        # do this only for English
+        if data_name.split("-")[0] == "en"
+    }
+)
+src2error = {src: len(grammarcheck.check(src)) / len(src.split()) for src in src2error}
+grammarcheck.close()
+
+# compute some protected attributes
+for data_name, data in list(data_all.lp2src_data_list.items()):
+    for line in tqdm.tqdm(data):
+        line["effect"] = {
+            "grammaticality": src2error[line["src"]],
+            "length": len(line["src"]),
+        }
+
+# %%
+
+# apply scorers to the whole data
+subsampling.sentinel.sentinel_src_metric_model_score(
+    subsampling.sentinel.get_sentinel_src_metric_model(
+        "Prosho/sentinel-src-mqm-wmt1723"
+    ),
+    scorer_name="sentinel-src-mqm-wmt1723",
+    data=data_all,
+    use_tgt_lang_token=False,
+)
+subsampling.misc.apply_subset2evaluate(data_all, method="random")
+subsampling.syntactic_complexity.syntactic_complexity_score(
+    data_all, "syntactic_complexity"
+)
+subsampling.syntactic_complexity.src_len_score(
+    data_all,
+    "src_len",
+)
+
+# %%
+
+
+data_size = len(list(data_all.lp2src_data_list.values())[0])
+
+
+def avg_effect_across_langs(key, en_only=False):
+    return [
+        data[i]["effect"][key]
+        for data_name, data in data_all.lp2src_data_list.items()
+        if (not en_only) or (data_name.split("-")[0] == "en")
+        for i in range(data_size)
+    ]
+
+
+def avg_difficulty_across_langs(key, en_only=False):
+    return [
+        np.average(
+            [
+                np.average(
+                    [data[i]["scores"][sys][key] for sys in data[i]["scores"].keys()]
+                )
+                for data in data_all.lp2src_data_list.values()
+                if (not en_only) or (data_name.split("-")[0] == "en")
+            ]
+        )
+        for i in range(data_size)
+    ]
+
+
+import collections
+
+METHOD_CORR = collections.defaultdict(dict)
+
+
+def plot_problem(ax, data_x, data_y, key_x, key_y):
+    # filter out None from data_y, but in parallel
+    data_x, data_y = zip(
+        *[(x, y) for x, y in zip(data_x, data_y) if x is not None and y is not None]
+    )
+    ax.scatter(
+        data_x,
+        data_y,
+        s=5,
+        color="black",
+        alpha=0.7,
+        linewidth=0,
+    )
+    ax.spines[["top", "right"]].set_visible(False)
+
+    corr_pearson = scipy.stats.pearsonr(data_x, data_y)[0]
+    corr_spearman = scipy.stats.spearmanr(data_x, data_y)[0]
+
+    if key_x == "random":
+        corr_pearson = 0.0
+        corr_spearman = 0.0
+
+    METHOD_CORR[key_y][key_x] = {
+        "pearson": corr_pearson,
+        "spearman": corr_spearman,
+    }
+    ax.text(
+        0.95,
+        0.05,
+        f"ρ={corr_pearson:.2f} (Pearson)\nρ={corr_spearman:.2f} (Spearman)",
+        transform=ax.transAxes,
+        fontsize=8,
+        ha="right",
+        va="bottom",
+    )
+
+
+METHOD_TO_NAME = {
+    "random": "Random",
+    "syntactic_complexity": "Syntactic Complexity",
+    "sentinel-src-mqm-wmt1723": "Sentinel",
+    "src_len": "Length",
+}
+
+fig, axss = plt.subplots(
+    len(METHOD_TO_NAME),
+    5,
+    figsize=(11, 2.1 * len(METHOD_TO_NAME)),
+    width_ratios=[1, 1, 1, 1, 1],
+)
+for axs_i, (axs, (method, method_name)) in enumerate(zip(axss, METHOD_TO_NAME.items())):
+    plot_problem(
+        axs[0],
+        avg_effect_across_langs("length"),
+        avg_difficulty_across_langs(method),
+        key_x="length (characters)",
+        key_y=method_name,
+    )
+
+    plot_problem(
+        axs[4],
+        avg_effect_across_langs("grammaticality", en_only=True),
+        avg_difficulty_across_langs(method),
+        key_x="grammar errors per word",
+        key_y=method_name,
+    )
+
+    axs[0].set_ylabel(method_name)
+
+    if axs_i == len(METHOD_TO_NAME) - 1:
+        axs[0].set_xlabel("Length")
+        axs[4].set_xlabel("Grammaticality")
+
+
+plt.tight_layout(pad=0)
+plt.savefig("../generated/03-post_effect_wmt25.pdf")
+plt.show()
+
+
+# %%
+
+fout = open("../generated/03-post_effect_corr_wmt25.tex", "w")
+
+
+print(
+    "\\begin{tabular}{l" + "r" * len(METHOD_CORR["Length"]) + "} \n \\toprule",
+    file=fout,
+)
+METHODNAME_TO_SHORT = {
+    "Random": "Random",
+    "Syntactic Complexity": "Syntax Complexity",
+    "Sentinel": "Sentinel",
+    "Length": "Length",
+}
+print(
+    r"""
+    & \multicolumn{2}{c}{\bf Source} & \multicolumn{2}{c}{\bf Diversity} & \bf Unique \\
+    & \bf length & \bf errors & \bf embd & \bf chrF\hspace{0.5mm} & \bf outputs \\
+    \midrule
+    """,
+    file=fout,
+)
+
+
+def format_cell(v, minv=0, maxv=1.1):
+    va = abs(v)
+    color = int(100 * (va - minv) / (maxv - minv))
+    return f"\\cellcolor{{red!{color}}} ${v:.2f}$"
+
+
+METHOD_CORR["Random"] = {
+    k: {"pearson": 0.0, "spearman": 0.0} for k in METHOD_CORR["Random"].keys()
+}
+
+KEYX = [
+    "length (characters)",
+    "grammar errors per word",
+]
+for method_name, method_v in METHOD_CORR.items():
+    print(f"{METHODNAME_TO_SHORT[method_name]} & ", file=fout)
+    print(
+        *[format_cell(method_v[k]["pearson"]) for k in KEYX],
+        sep=" & ",
+        end="\\\\\n",
+        file=fout,
+    )
+
+print("\\bottomrule ", file=fout)
+print("\\end{tabular}", file=fout)
+fout.close()
---
+++ experiments/05-post_effect_src_diversity_wmt25.py
@@ -0,0 +1,263 @@
+# %%
+import json
+import sentence_transformers.util
+import difficulty_sampling
+import difficulty_sampling.evaluate
+import difficulty_sampling.utils
+import difficulty_sampling.data
+import subsampling.sentinel
+import subsampling.syntactic_complexity
+import subsampling.average_word_frequency
+import subsampling.misc
+import sentence_transformers
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+with open("../data/wmt25.jsonl", "r") as f:
+    data_raw = [
+        {
+            "src": json.loads(x),
+            "scores": {"mock": {}},
+        }
+        for x in f.readlines()
+    ]
+
+data_all = difficulty_sampling.data.Data(
+    lp2src_data_list={"en": data_raw},
+    lps=["en"],
+    dataset_name="wmt25",
+    protocol=None,
+    domains="news",
+)
+
+
+# %%
+
+# apply scorers to the whole data
+subsampling.sentinel.sentinel_src_metric_model_score(
+    subsampling.sentinel.get_sentinel_src_metric_model(
+        "Prosho/sentinel-src-mqm-wmt1723"
+    ),
+    scorer_name="sentinel-src-mqm-wmt1723",
+    data=data_all,
+    use_tgt_lang_token=False,
+)
+subsampling.misc.apply_subset2evaluate(data_all, method="random")
+subsampling.syntactic_complexity.syntactic_complexity_score(
+    data_all, "syntactic_complexity"
+)
+subsampling.syntactic_complexity.src_len_score(
+    data_all,
+    "src_len",
+)
+
+# %%
+# embedd all sources
+# map tgt to embedding
+src2embd = list(
+    {line["src"] for data in data_all.lp2src_data_list.values() for line in data}
+)
+src2embd = dict(
+    zip(
+        src2embd,
+        sentence_transformers.SentenceTransformer(
+            "paraphrase-multilingual-MiniLM-L12-v2"
+        ).encode(src2embd),
+    )
+)
+
+# %%
+
+data_x = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+
+def measure_avg_sim(method_name, p: float):
+    mean_dists = []
+    for data in data_all.lp2src_data_list.values():
+        # take top-B from data based on data_y
+        B = int(len(data) * p)
+        data_y = [
+            np.average([x["scores"][sys][method_name] for sys in x["scores"].keys()])
+            for x in data
+        ]
+        data_local = [
+            x[0] for x in sorted(list(zip(data, data_y)), key=lambda x: x[1])[:B]
+        ]
+
+        # measure average point distance based on embedding
+        data_embd = [src2embd[line["src"]] for line in data_local]
+        data_embd = np.array(data_embd)
+        # measure distance
+        mean_dist = sentence_transformers.util.cos_sim(data_embd, data_embd).mean()
+        mean_dists.append(mean_dist)
+    return np.average(mean_dists)
+
+
+METHOD_TO_NAME = {
+    "random": "Random",
+    "syntactic_complexity": "Syntax Complexity",
+    "sentinel-src-mqm-wmt1723": "Sentinel",
+    "src_len": "Length",
+}
+
+
+def measure_closest_sim(method_name, p: float):
+    closest_dists = []
+    for data in data_all.lp2src_data_list.values():
+        # take top-B from data based on data_y
+        B = int(len(data) * p)
+        data_y = [
+            np.average([x["scores"][sys][method_name] for sys in x["scores"].keys()])
+            for x in data
+        ]
+        data_local = [
+            x[0] for x in sorted(list(zip(data, data_y)), key=lambda x: x[1])[:B]
+        ]
+
+        # measure average point distance based on embedding
+        data_embd = [src2embd[line["src"]] for line in data_local]
+        data_embd = np.array(data_embd)
+        # measure distance
+        dists = sentence_transformers.util.cos_sim(data_embd, data_embd)
+        # take closest point for each point
+        dists = dists - np.eye(dists.shape[0])
+        dists = np.max(dists.numpy(), axis=1)
+        closest_dists.append(dists.mean())
+    return np.average(closest_dists)
+
+
+results_avg = collections.defaultdict(list)
+results_avg_random = collections.defaultdict(list)
+results_closest = collections.defaultdict(list)
+results_closest_random = collections.defaultdict(list)
+for p in data_x:
+    for method in METHOD_TO_NAME.keys():
+        results_avg[method].append(measure_avg_sim(method, p))
+        results_closest[method].append(measure_closest_sim(method, p))
+
+for i in range(10):
+    subsampling.misc.apply_subset2evaluate(data_all, method="random")
+    for p in data_x:
+        results_avg_random[i].append(measure_avg_sim("random", p))
+        results_closest_random[i].append(measure_closest_sim("random", p))
+
+
+# %%
+
+METHOD_TO_COLOR = {
+    "random": "black",
+    "src_len": "#a0a010",
+    "syntactic_complexity": difficulty_sampling.utils.COLORS[3],
+    "sentinel-src-mqm-wmt1723": difficulty_sampling.utils.COLORS[2],
+}
+
+difficulty_sampling.utils.matplotlib_default()
+
+fig, axs = plt.subplots(1, 2, figsize=(7.5, 2.5))
+
+# plot closest
+data_y_rand_closets_interval = [
+    difficulty_sampling.utils.confidence_interval(l, confidence=0.99)
+    for l in np.array(list(results_closest_random.values())).T
+]
+axs[0].fill_between(
+    data_x,
+    [x[0] for x in data_y_rand_closets_interval],
+    [x[1] for x in data_y_rand_closets_interval],
+    color=METHOD_TO_COLOR["random"],
+    linewidth=0,
+    alpha=0.4,
+    zorder=-10,
+    label="Random",
+)
+for method in METHOD_TO_COLOR.keys():
+    results_v = results_closest[method]
+    if method == "random":
+        continue
+    axs[0].plot(
+        data_x,
+        results_v,
+        label=METHOD_TO_NAME[method],
+        color=METHOD_TO_COLOR[method],
+        linewidth=2,
+    )
+
+# plot avg
+data_y_rand_avg_interval = [
+    difficulty_sampling.utils.confidence_interval(l, confidence=0.99)
+    for l in np.array(list(results_avg_random.values())).T
+]
+axs[1].fill_between(
+    data_x,
+    [x[0] for x in data_y_rand_avg_interval],
+    [x[1] for x in data_y_rand_avg_interval],
+    color=METHOD_TO_COLOR["random"],
+    linewidth=0,
+    alpha=0.4,
+    zorder=-10,
+)
+for method in METHOD_TO_COLOR.keys():
+    results_v = results_avg[method]
+    if method == "random":
+        continue
+    axs[1].plot(
+        data_x,
+        results_v,
+        label=METHOD_TO_NAME[method],
+        color=METHOD_TO_COLOR[method],
+        linewidth=2,
+    )
+
+# configure decorations
+axs[0].spines[["top", "right"]].set_visible(False)
+axs[1].spines[["top", "right"]].set_visible(False)
+
+axs[0].set_xticks(data_x[::2])
+axs[0].set_xticklabels([f"{int(p*100)}%" for p in data_x[::2]])
+axs[1].set_xticks(data_x[::2])
+axs[1].set_xticklabels([f"{int(p*100)}%" for p in data_x[::2]])
+
+# set formatter to three decimal places
+axs[0].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.3f}"))
+axs[1].yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.3f}"))
+
+axs[0].set_ylabel("Closest Neighbour\nCosine Similarity")
+axs[1].set_ylabel("Average Pairwise\nCosine Similarity")
+axs[0].set_xlabel("Proportion of original data")
+axs[1].set_xlabel("Proportion of original data")
+axs[0].set_ylim(0.434, None)
+
+handles, handles_txt = plt.gca().get_legend_handles_labels()
+
+plt.tight_layout(pad=0)
+# negative spacing between subplots
+plt.subplots_adjust(wspace=0.25)
+plt.savefig("../generated/05-post_effect_src_diversity_wmt25.pdf")
+plt.show()
+
+# %%
+
+# fix the line for Random
+import copy
+
+handle_random = copy.deepcopy(handles[0])
+handle_random.set_color(METHOD_TO_COLOR["random"])
+
+# plot just the legend
+plt.figure(figsize=(7.5, 0.4))
+plt.legend(
+    [handle_random] + handles,
+    ["Random"] + handles_txt,
+    loc="center",
+    fontsize=9,
+    ncol=7,
+    frameon=False,
+    handlelength=0.7,
+    handletextpad=0.5,
+    columnspacing=0.9,
+)
+plt.axis("off")
+plt.savefig("../generated/04-variable_budget_legend_wmt25.pdf")
+plt.tight_layout()
